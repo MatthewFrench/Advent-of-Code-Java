d20fab163946bb70bb1a0e0ca2a05e837ed4bd31
MatthewFrench <9086152+MatthewFrench@users.noreply.github.com>
2023-12-08 18:17:55 -0600

    Added efficient solution for day 5

--- app/src/main/java/Advent/of/Code/Java/Year_2023/Day_5.java
+++ app/src/main/java/Advent/of/Code/Java/Year_2023/Day_5.java
@@ -24,7 +24,7 @@ public class Day_5 implements DayWithExecute {
 
     public void executeWithInput(final String fileName) throws Exception {
         runSolution1(fileName);
-        runSolution2(fileName);
+        runSolution2Efficient(fileName);
     }
     final static NumberFormat myFormat = NumberFormat.getInstance();
 
@@ -55,7 +55,99 @@ public class Day_5 implements DayWithExecute {
         LogUtilities.logGreen("Solution 1: " + minimumValue);
     }
 
-    private void runSolution2(final String fileName) throws Exception {
+    class Range {
+        long start;
+        long end;
+    }
+
+    private void runSolution2Efficient(final String fileName) throws Exception {
+        final List<String> input = LoadUtilities.loadTextFileAsList(fileName);
+
+        // Parse seeds
+        List<Range> ranges = new ArrayList<>();
+        {
+            final List<String> seedValues = StringUtilities.splitStringIntoList(StringUtilities.splitStringIntoList(input.get(0), ": ").get(1), " ");
+            for (int index = 0; index < seedValues.size(); index+=2) {
+                final Range range = new Range();
+                range.start = Long.parseLong(seedValues.get(index));
+                range.end = range.start + Long.parseLong(seedValues.get(index+1));
+                ranges.add(range);
+            }
+        }
+
+        // Parse mappings
+        final Map<String, Mapping> mappings = getMappings(input);
+
+        // Now transform and subdivide each range with intersections on the mappings
+        for (final String mapName : MAPPING_ORDER) {
+            final Mapping mapping = mappings.get(mapName);
+
+            final List<Range> newRanges = new ArrayList<>();
+
+            for (final Range range : ranges) {
+                // Note: The intersecting ranges should already be in sorted order
+                final List<MappingRange> intersectingRanges = getIntersectingRanges(range.start, range.end, mapping.mappingRanges);
+                long start = range.start;
+                long end = range.end;
+                for (final MappingRange mappingRange : intersectingRanges) {
+                    if (start < mappingRange.sourceStart) {
+                        // Cut the first part of the range off that doesn't intersect
+                        final Range newRange = new Range();
+                        newRange.start = start;
+                        newRange.end = mappingRange.sourceStart;
+                        start = mappingRange.sourceStart;
+                        newRanges.add(newRange);
+                    }
+                    if (start < mappingRange.sourceEnd) {
+                        if (end <= mappingRange.sourceEnd) {
+                            // The rest of the range is entirely within the mapping range
+                            final Range newRange = new Range();
+                            newRange.start = start - mappingRange.sourceStart + mappingRange.destinationStart;
+                            newRange.end = end - mappingRange.sourceStart + mappingRange.destinationStart;
+                            newRanges.add(newRange);
+                            // There is no more range for other mapping ranges to use, this should only happen on the last item in this loop
+                            start = end;
+                            break;
+                        } else {
+                            // The end is outside the mapping range
+                            final Range newRange = new Range();
+                            newRange.start = start - mappingRange.sourceStart + mappingRange.destinationStart;
+                            newRange.end = mappingRange.destinationEnd;
+                            // Move the start to be outside of the mapping range
+                            start = mappingRange.sourceEnd;
+                            newRanges.add(newRange);
+                        }
+                    }
+                }
+                if (start != end) {
+                    final Range newRange = new Range();
+                    newRange.start = start;
+                    newRange.end = end;
+                    newRanges.add(newRange);
+                }
+            }
+            ranges = newRanges;
+        }
+
+        ranges.sort(Comparator.comparingLong(o -> o.start));
+
+        LogUtilities.logGreen("Solution 2 efficient: " + ranges.get(0).start);
+    }
+
+    private List<MappingRange> getIntersectingRanges(final long start, final long end, final List<MappingRange> mappingRanges) {
+        final List<MappingRange> intersectingRanges = new ArrayList<>();
+        for (final MappingRange mappingRange : mappingRanges) {
+            if ((start <= mappingRange.sourceStart && end >= mappingRange.sourceEnd)
+                    || (start >= mappingRange.sourceStart && start < mappingRange.sourceEnd)
+                    || (end > mappingRange.sourceStart && end <= mappingRange.sourceEnd)
+            ) {
+                intersectingRanges.add(mappingRange);
+            }
+        }
+        return intersectingRanges;
+    }
+
+    private void runSolution2BruteForce(final String fileName) throws Exception {
         final List<String> input = LoadUtilities.loadTextFileAsList(fileName);
 
         AtomicReference<Long> minimumSeedLocation = new AtomicReference<>(null);
